9ba13cbec813101fb434148abfcfdcf850590046
asjay <sejin1314@korea.ac.kr>
2023-10-10 21:42:58 +0900

    Upload Chapter2 Solutions

--- HW/Chapter2/Exercise1/cases.js
+++ HW/Chapter2/Exercise1/cases.js
@@ -1,1 +1,21 @@
 // your code
+
+const factorial = (n) => {
+  var answer = 1;
+  for (let index = 1; index <= n; index++) {
+    answer *= index;
+  }
+  return answer;
+};
+
+const permutation = (n, r) => factorial(n) / factorial(n - r);
+const combination = (n, r) => permutation(n, r) / factorial(r);
+const multiPermutation = (n, r) => n ** r;
+const multiCombination = (n, r) => combination(n + r - 1, r);
+
+module.exports = {
+  permutation,
+  combination,
+  multiPermutation,
+  multiCombination,
+};
--- HW/Chapter2/Exercise2/index.2.2.js
+++ HW/Chapter2/Exercise2/index.2.2.js
@@ -1,1 +1,26 @@
-// your code
+const fs = require("fs");
+const path = require("path");
+
+const util = require("util");
+const getDirList = util.promisify(fs.readdir);
+const getFileStat = util.promisify(fs.stat);
+
+const searchDirectory = async (directory) => {
+  const files = await getDirList(directory);
+  files.forEach(async (file) => {
+    const filePath = path.join(directory, file);
+    const stat = await getFileStat(filePath);
+    if (stat.isDirectory()) await searchDirectory(filePath);
+    else if (path.extname(filePath) === ".js") console.log(filePath);
+  });
+};
+
+const PATH = "./test";
+
+(async () => {
+  try {
+    await searchDirectory(PATH);
+  } catch (err) {
+    console.error(err);
+  }
+})();
