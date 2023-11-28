908f35c577250b3cb1b84f756ffa1b554d6ecc9d
asjay <sejin1314@korea.ac.kr>
2023-11-28 20:17:45 +0900

    finish chapter7.3

--- Chapter7/node-simple-board-skeleton/src/controller/article/ctrl.js
+++ Chapter7/node-simple-board-skeleton/src/controller/article/ctrl.js
@@ -39,3 +39,62 @@ const writeArticle = async (req, res, next) => {
     next(err);
   }
 };
+
+const editArticleForm = async (req, res, next) => {
+  try {
+    const { articleId } = req.params;
+    const { user } = req.session;
+
+    const article = await ArticleDAO.getById(articleId);
+    if (!article) throw new Error("NOT_EXIST");
+
+    return res.render("articles/editor.pug", { user, article });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const editArticle = async (req, res, next) => {
+  try {
+    const { articleId } = req.params;
+    const { user } = req.session;
+    const title = req.body.title.trim();
+    const content = req.body.content.trim();
+    if (!title || !content || title.length > 50 || content.length > 65000) {
+      throw new Error("BAD_REQUEST");
+    }
+
+    const article = await ArticleDAO.getByIdAndAuthor(articleId, user);
+    if (!article) throw new Error("NOT_EXIST");
+
+    await ArticleDAO.update(articleId, title, content);
+    return res.redirect(`/article/${articleId}`);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteArticle = async (req, res, next) => {
+  try {
+    const { articleId } = req.params;
+    const { user } = req.session;
+    const article = await ArticleDAO.getByIdAndAuthor(articleId, user);
+
+    if (!article) throw new Error("NOT_EXIST");
+
+    await ArticleDAO.remove(articleId);
+
+    return res.redirect("/articles/page/1");
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  readArticle,
+  writeArticleForm,
+  writeArticle,
+  editArticleForm,
+  editArticle,
+  deleteArticle,
+};
